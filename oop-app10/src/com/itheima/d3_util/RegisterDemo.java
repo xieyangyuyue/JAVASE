f4038606ecc3e86bb1cdae1121cfb9d0e7c63558
xieyangyuyue <1483618794@qq.com>
2024-12-22 19:06:06 +0800

    [add]day10-面向对象高级1

---
+++ oop-app10/src/com/itheima/d3_util/RegisterDemo.java
@@ -0,0 +1,10 @@
+package com.itheima.d3_util;
+
+/**
+  注册界面
+ */
+public class RegisterDemo {
+    public static void main(String[] args) {
+        System.out.println(MyUtil.createCode(6));
+    }
+}
