da2f47efc7e7934d90f1d77b6780d6415781edb9
xieyangyuyue <1483618794@qq.com>
2025-01-07 18:50:47 +0800

    [add]day13-常用API：BigDecimal，JDK8新时间等、Lambda、方法引用

--- oop-app12/src/com/itheima/d13_stringBuilder/Test2.java
+++ oop-app12/src/com/itheima/d13_stringBuilder/Test2.java
@@ -5,17 +5,17 @@ public class Test2 {
         // 目标：掌握StringBuilder的好处。
         // 需求：要拼接100万次abc
         // 先用String测试看看性能
-//        String rs = "";
-//        for (int i = 1; i <= 1000000 ; i++) {
-//            rs = rs + "abc";
-//        }
-//        System.out.println(rs);
-
-        // 使用StringBuilder演示
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= 1000000; i++) {
-            sb.append("abc");
+        String rs = "";
+        for (int i = 1; i <= 1000000 ; i++) {
+            rs = rs + "abc";
         }
-        System.out.println(sb);
+        System.out.println(rs);
+
+//        // 使用StringBuilder演示
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 1; i <= 1000000; i++) {
+//            sb.append("abc");
+//        }
+//        System.out.println(sb);
     }
 }
